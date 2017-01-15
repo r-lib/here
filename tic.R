1f04f50fd8ba4e6d1ec23b82d2b523112f21cd72
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-15 16:21:13 +0100

    use tic

--- tic.R
+++ tic.R
@@ -1,11 +1,13 @@
-after_success <- list(
-  step(task_hello_world),
-  step(task_run_covr)
-)
+get_stage("after_success") %>%
+  add_step(step_hello_world()) %>%
+  add_step(step_run_covr())
 
-deploy <- list(
-  step(task_build_pkgdown, on_branch = "production", on_env = "BUILD_PKGDOWN"),
-  step(task_install_ssh_keys),
-  step(task_test_ssh),
-  step(task_push_deploy, path = "docs", branch = "gh-pages", on_branch = "production", on_env = "BUILD_PKGDOWN")
-)
+get_stage("deploy") %>%
+  add_step(step_install_ssh_keys()) %>%
+  add_step(step_test_ssh())
+
+if (ci()$get_branch() == "production" && Sys.getenv("BUILD_PKGDOWN") != "") {
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+}

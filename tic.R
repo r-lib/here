e29e741fa64796fcb7b5e63f958978f4ed0edb38
Kirill Müller <krlmlr+r@mailbox.org>
2018-03-28 21:01:03 +0200

    oops

--- tic.R
+++ tic.R
@@ -1,8 +1,5 @@
 add_package_checks()
 
-get_stage("deploy") %>%
-  add_step(step_build_pkgdown())
-
 if (Sys.getenv("BUILD_PKGDOWN") != "" && Sys.getenv("id_rsa") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh()) %>%
@@ -15,5 +12,6 @@ if (Sys.getenv("BUILD_PKGDOWN") != "" && Sys.getenv("id_rsa") != "") {
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("deploy") %>%
     add_code_step(TRUE, prepare_call = remotes::install_github("tidyverse/tidytemplate")) %>%
+    add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(subdir = "docs"))
 }

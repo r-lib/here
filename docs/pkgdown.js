e5ddac52a830493b440f22616891a2aef3f97ada
Kirill Müller <krlmlr+r@mailbox.org>
2018-03-28 19:02:23 +0000

    Deploy from Travis build 111 [ci skip]
    Build URL: https://travis-ci.org/r-lib/here/builds/359514349
    Commit: 34cac4b8ad9f29e1548db42a38f19294e18354e3

--- docs/pkgdown.js
+++ docs/pkgdown.js
@@ -1,20 +1,10 @@
 $(function() {
-
-  $("#sidebar")
-    .stick_in_parent({offset_top: 40})
-    .on('sticky_kit:bottom', function(e) {
-      $(this).parent().css('position', 'static');
-    })
-    .on('sticky_kit:unbottom', function(e) {
-      $(this).parent().css('position', 'relative');
-    });
-
-  $('body').scrollspy({
-    target: '#sidebar',
-    offset: 60
+  $("#sidebar").stick_in_parent({
+    offset_top: $("#sidebar").offset().top
   });
-
-  $('[data-toggle="tooltip"]').tooltip();
+  $('body').scrollspy({
+    target: '#sidebar'
+  });
 
   var cur_path = paths(location.pathname);
   $("#navbar ul li a").each(function(index, value) {
@@ -31,7 +21,10 @@ $(function() {
       menu_anchor.closest("li.dropdown").addClass("active");
     }
   });
+
 });
+
+
 
 function paths(pathname) {
   var pieces = pathname.split("/");
@@ -47,11 +40,6 @@ function is_prefix(needle, haystack) {
   if (needle.length > haystack.lengh)
     return(false);
 
-  // Special case for length-0 haystack, since for loop won't run
-  if (haystack.length === 0) {
-    return(needle.length === 0);
-  }
-
   for (var i = 0; i < haystack.length; i++) {
     if (needle[i] != haystack[i])
       return(false);
@@ -59,43 +47,3 @@ function is_prefix(needle, haystack) {
 
   return(true);
 }
-
-/* Clipboard --------------------------*/
-
-function changeTooltipMessage(element, msg) {
-  var tooltipOriginalTitle=element.getAttribute('data-original-title');
-  element.setAttribute('data-original-title', msg);
-  $(element).tooltip('show');
-  element.setAttribute('data-original-title', tooltipOriginalTitle);
-}
-
-if(Clipboard.isSupported()) {
-  $(document).ready(function() {
-    var copyButton = "<button type='button' class='btn btn-primary btn-copy-ex' type = 'submit' title='Copy to clipboard' aria-hidden='true' data-toggle='tooltip' data-placement='left auto' data-trigger='hover' data-clipboard-copy><i class='fa fa-copy' aria-hidden='true'></i></button>";
-
-    $(".examples").addClass("hasCopyButton");
-
-    // Insert copy buttons:
-    $(copyButton).prependTo(".hasCopyButton");
-
-    // Initialize tooltips:
-    $('.btn-copy-ex').tooltip({container: 'body'});
-
-    // Initialize clipboard:
-    var clipboardBtnCopies = new Clipboard('[data-clipboard-copy]', {
-      text: function(trigger) {
-        return trigger.parentNode.textContent;
-      }
-    });
-
-    clipboardBtnCopies.on('success', function(e) {
-      changeTooltipMessage(e.trigger, 'Copied!');
-      e.clearSelection();
-    });
-
-    clipboardBtnCopies.on('error', function() {
-      changeTooltipMessage(e.trigger,'Press Ctrl+C or Command+C to copy');
-    });
-  });
-}
-

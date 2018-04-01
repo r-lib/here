67d6ff1a425308bdd4d7be3cc1f4afe182b41314
Kirill Müller <krlmlr+r@mailbox.org>
2018-04-01 12:24:02 +0200

    new tic rules

--- tic.R
+++ tic.R
@@ -1,17 +1,23 @@
 add_package_checks()
 
-if (Sys.getenv("BUILD_PKGDOWN") != "" && Sys.getenv("id_rsa") != "") {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh()) %>%
-    add_step(step_test_ssh())
+if (Sys.getenv("BUILD_PKGDOWN") != "" && !ci()$is_tag()) {
+  get_stage("install") %>%
+    add_code_step(remotes::install_github("tidyverse/tidytemplate"))
 
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("deploy") %>%
-    add_code_step(TRUE, prepare_call = remotes::install_github("tidyverse/tidytemplate")) %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(subdir = "docs"))
+    add_step(step_build_pkgdown())
+
+  if (Sys.getenv("id_rsa") != "") {
+    get_stage("before_deploy") %>%
+      add_step(step_setup_ssh()) %>%
+      add_step(step_test_ssh())
+
+    # pkgdown documentation can be built optionally. Other example criteria:
+    # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+    # - `ci()$is_tag()`: Only for tags, not for branches
+    # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+    # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+    get_stage("deploy") %>%
+      add_step(step_push_deploy(subdir = "docs"))
+  }
 }

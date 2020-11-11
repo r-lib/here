3888116392c5f3237e857f9aa3e53285004d8dc5
Kirill Müller <krlmlr+r@mailbox.org>
2020-11-11 05:41:18 +0100

    Styler

--- R/here-dr.R
+++ R/here-dr.R
@@ -12,8 +12,10 @@ format_reason <- function() {
   if (any(vapply(.root_env$crit$testfun, function(f) f(root), logical(1L)))) {
     paste0("it ", get_root_desc(.root_env$crit, .root_env$f()))
   } else {
-    paste0("none of the following criteria apply for this directory or any of its parents:\n",
-           format_root_criteria_items(), "\n",
-           "Use set_here() to create a `.here` file")
+    paste0(
+      "none of the following criteria apply for this directory or any of its parents:\n",
+      format_root_criteria_items(), "\n",
+      "Use set_here() to create a `.here` file"
+    )
   }
 }

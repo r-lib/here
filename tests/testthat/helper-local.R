c7f129eeb8f2460ae586b965555ca608bd98ac3b
Kirill Müller <krlmlr+r@mailbox.org>
2020-11-13 04:51:30 +0100

    Test dr_here()

--- tests/testthat/helper-local.R
+++ tests/testthat/helper-local.R
@@ -1,15 +1,22 @@
-local_project <- function(.env = parent.frame()) {
+local_project <- function(..., .env = parent.frame()) {
   path <- tempfile()
   dir.create(path)
+
   file.copy(
     dir(system.file("demo-project", package = "here"), full.names = TRUE),
     path,
     recursive = TRUE
   )
 
-  do_refresh_here(path)
+  local_here(path, .env = .env)
 
-  withr::defer(do_refresh_here("."), .env)
   withr::defer(unlink(path, recursive = TRUE), .env)
   path
+}
+
+local_here <- function(path, ..., .env = parent.frame()) {
+  old_here <- here()
+  do_refresh_here(path)
+  withr::defer(do_refresh_here(old_here), .env)
+  invisible()
 }
---
+++ tests/testthat/test-dr_here.R
@@ -0,0 +1,25 @@
+local_project()
+
+test_that("dr_here() shows message", {
+  expect_message(dr_here(), "starts at")
+})
+
+test_that("format_dr_here(show_reason = TRUE)", {
+  dr <- format_dr_here(show_reason = TRUE)
+  dr <- gsub(here(), "<project root>", dr, fixed = TRUE)
+  expect_snapshot(writeLines(dr))
+})
+
+test_that("format_dr_here(show_reason = FALSE)", {
+  dr <- format_dr_here(show_reason = FALSE)
+  dr <- gsub(here(), "<project root>", dr, fixed = TRUE)
+  expect_snapshot(writeLines(dr))
+})
+
+test_that("format_dr_here() without root", {
+  local_here(tempdir())
+
+  dr <- format_dr_here(show_reason = TRUE)
+  dr <- gsub(here(), "<project root>", dr, fixed = TRUE)
+  expect_snapshot(writeLines(dr))
+})

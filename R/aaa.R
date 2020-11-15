5a8d4c0149fbfe211e0960cfce427fef74c6ff9b
Kirill Müller <krlmlr+r@mailbox.org>
2020-11-15 05:46:06 +0100

    Project root section

--- R/aaa.R
+++ R/aaa.R
@@ -2,10 +2,17 @@
 format_root_section <- function() {
   paste(
     "\\section{Project root}{",
-    "Starting with the current working directory during package load time, `here` will walk the directory hierarchy upwards until it finds a directory that satisfies at least one of the following conditions:",
+    "The project root is established with a call to [here::i_am()].",
+    "Although not recommended, it can be changed by calling `here::i_am()` again.",
+    "",
+    "In the absence of such a call (e.g. for a new project),",
+    "starting with the current working directory during package load time,",
+    "the directory hierarchy is walked upwards ",
+    "until a directory with at least one of the following conditions is found:",
     format_root_criteria_items(),
     "",
-    "Once established, the root directory doesn't change during the active R session. `here()` then appends the arguments to the root directory.",
+    "In either case, `here()` appends its arguments as path components ",
+    "to the root directory.",
     "}",
     sep = "\n"
   )
@@ -13,5 +20,6 @@ format_root_section <- function() {
 # nocov end
 
 format_root_criteria_items <- function() {
-  paste(format(.root_env$root$crit)[-1L], collapse = "\n")
+  format <- gsub('"([^"]+)"', "`\\1`", format(.root_env$root$crit)[-1L])
+  paste(format, collapse = "\n")
 }
